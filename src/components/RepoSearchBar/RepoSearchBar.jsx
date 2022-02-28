b84145004d17521d6f678529b372816eb782f5e4
Kae <kaehehehe@gmail.com>
2022-03-01 08:14:12 +0900

    Refactor: api와 통신하는 코드를 searchBar 컴포넌트에 이동

--- src/components/RepoSearchBar/RepoSearchBar.jsx
+++ src/components/RepoSearchBar/RepoSearchBar.jsx
@@ -1,15 +1,33 @@
 import React, { useEffect, useRef } from "react";
 import { FaSearch } from "react-icons/fa";
+import axios from "axios";
 
 import * as S from "./style";
 
 function RepoSearchBar({
-  getRepositoryData,
   endView,
   setEndView,
   setLoadingState,
+  setRepositoryList,
 }) {
   const searchWordInputRef = useRef("");
+
+  const getRepositoryData = async inputValue => {
+    try {
+      const res = await axios.get(
+        "https://api.github.com/search/repositories",
+        {
+          params: {
+            q: inputValue,
+          },
+        },
+      );
+      setRepositoryList(res.data.items);
+      setLoadingState(0);
+    } catch (err) {
+      console.error(Error);
+    }
+  };
 
   useEffect(() => {
     searchWordInputRef.current.focus();
